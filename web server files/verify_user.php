5d7e5bdfcbaada976a70f3a242621387451cb5ca
Leo <leomed97@gmail.com>
2015-09-27 04:15:10 -0400

    fixed the verify_user php file

--- web server files/verify_user.php
+++ web server files/verify_user.php
@@ -2,12 +2,38 @@
 require_once('connection.php');
 
 
-$user_ID = $_POST['user_id'];
-$user_name = $_POST['user_name'];
-$user_email = $_POST['user_email'];
-$user_password = "SELECT password FROM Users
-WHERE name = '$user_name'";
-$password_correct = password_verify($_POST['password'], $user_password);
+
+if (!empty($_POST)){
+    if (empty($_POST['user_name'] || empty($_POST['password'] ))){
+        $response['success']=0;
+        $message['message']="All fields must be filled out";
+        die(json_encode($response));
+
+    }
+
+
+
+    $user_name = $_POST['name'];
+    $name = "SELECT name FROM Users
+    WHERE name =  '$user_name'";
+
+    $user_password = "SELECT password FROM Users
+    WHERE name = '$user_name'";
+    $password_correct = password_verify($_POST['password'], $user_password);
+
+    if(!$password_correct){
+        $response['success']=0;
+        $message['message']="Password is incorrect";
+        die(json_encode($response));
+    }
+    else{
+        $response['success']=1;
+        $message['message']="You have been successfully logged in.";
+        die(json_encode($response));
+    }
+
+}
+
 
 
 
