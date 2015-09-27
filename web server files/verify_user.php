61c984cd7d9eab3f609d037df4ca73fce19128bf
Leo <leomed97@gmail.com>
2015-09-27 03:45:34 -0400

    fixed the verify_user php file

--- web server files/verify_user.php
+++ web server files/verify_user.php
@@ -5,7 +5,9 @@ require_once('connection.php');
 $user_ID = $_POST['user_id'];
 $user_name = $_POST['user_name'];
 $user_email = $_POST['user_email'];
-$password_correct = password_verify($_POST['password'], password_hash($_POST['password'], PASSWORD_DEFAULT));
+$user_password = "SELECT password FROM Users
+WHERE name = '$user_name'";
+$password_correct = password_verify($_POST['password'], $user_password);
 
 
 
